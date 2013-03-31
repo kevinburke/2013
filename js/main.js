de26a968d4270b97abb122afda2834801b1cb15e
Kevin Burke <kevin@twilio.com>
2013-03-31 12:39:11 -0700

    dont collapse the div.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-///DOC READY FUNCTIONS
-jQuery( document ).ready(function( $ ) {
-    if ( jQuery( '.accordion' )[0] ){
-        jQuery( '.accordion' ).find( 'h3' ).click( function( e ){
+jQuery(document).ready(function($) {
+    if (jQuery('.accordion')[0]) {
+        jQuery('.accordion').find('h3').click(function(e) {
             e.preventDefault();
-            jQuery( this ).next().slideToggle( 'fast' ).css( 'zoom', '1' );
-            jQuery( this ).parent( 'li' ).toggleClass( 'collapse' );
-            jQuery( this ).siblings( 'ul' ).toggleClass( 'hidden-phone' );
+            if (jQuery(this).siblings('.accordion-items').is(':visible')) {
+                // wide view enabled. return
+            }
+            jQuery(this).next().slideToggle('fast').css('zoom', '1');
+            jQuery(this).siblings('ul').toggleClass('hidden-phone');
         } );
     }
 } );
