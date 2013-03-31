64728748f04fa6d970985e89683a42895260f11f
Kevin Burke <kevin@twilio.com>
2013-03-30 22:34:33 -0700

    add basic accordion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+///DOC READY FUNCTIONS
+jQuery( document ).ready(function( $ ) {
+    if ( jQuery( '.accordion' )[0] ){
+        jQuery( '.accordion' ).find( 'h3' ).click( function( e ){
+            e.preventDefault();
+            jQuery( this ).next().slideToggle( 'fast' ).css( 'zoom', '1' );
+            jQuery( this ).parent( 'li' ).toggleClass( 'collapse' );
+        } );
+        jQuery( '.accordion > li' ).each( function( $ ){
+            if ( !jQuery( this ).hasClass( 'extended' ) ){
+                jQuery( this ).find( 'ul' ).slideUp( 'fast' ).css( 'zoom', '1' );
+                jQuery( this ).toggleClass( 'collapse' );
+            }
+        } );
+    }
+} );
