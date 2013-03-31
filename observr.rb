e14dd3091746f4d41183a3a926eb1821c8d88f3d
Kevin Burke <kevin@twilio.com>
2013-03-30 22:34:41 -0700

    compress the available css

--- observr.rb
+++ observr.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env observr
 
-watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass #{md[0]} > css/#{md[1]}.css"; puts cmd; system(cmd) }
+watch( 'scss/(.*)\.scss' )  {|md| cmd = "sass --style compressed #{md[0]} > css/#{md[1]}.css"; puts cmd; system(cmd) }
 
