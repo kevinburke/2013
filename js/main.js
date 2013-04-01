e78cd22831abf7b3c7d26b7367fba1fa610b3108
Kevin Burke <kevin@twilio.com>
2013-03-31 17:15:43 -0700

    Add Twitter integration CSS/SCSS/JS.
    Doing horrible things to Twitter's brand guidelines. Filthy things

--- js/main.js
+++ js/main.js
@@ -4,8 +4,28 @@ jQuery(document).ready(function($) {
             e.preventDefault();
             var h3 = jQuery(this);
             h3.next().slideToggle('slow', function() {
-                h3.siblings('ul').toggleClass('hidden-phone');
+                h3.siblings('ul, iframe').toggleClass('hidden-phone');
             }).css('zoom', '1');
         } );
     }
-} );
+
+});
+
+var checkTwitterFrame = function(callback) {
+    if ($('.twitter-timeline-rendered').length > 0) {
+        callback();
+    } else {
+        setTimeout(function() {
+            checkTwitterFrame(callback);
+        }, 100);
+    }
+};
+
+checkTwitterFrame(function() {
+    $('.twitter-timeline-rendered').addClass('hidden-phone');
+});
+
+var options = {
+    "url": "css/twitter.css"
+};
+CustomizeTwitterWidget(options);
