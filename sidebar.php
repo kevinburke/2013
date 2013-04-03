3b937f36a6ee74811b550ccfb3d3f54003bcb571
Kevin Burke <kevin@twilio.com>
2013-04-03 02:18:47 -0700

    add new blog menu to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,11 @@
-<div class="span3">
-<?php include_once('blurb.html'); ?>
+<div class="span3 sidebar">
+	<?php include_once('blurb.html'); ?>
+	<ul class="link-menu hidden-phone">
+		<li class="yellow"><a href="https://twitter.com/ekrubnivek"><div class="genericon genericon-twitter"></div>Twitter</a>
+		<li class="purple"><a href="https://github.com/kevinburke"><div class="genericon genericon-github"></div>Github</a>
+		<li class="orange"><a href="http://feeds.feedburner.com/kburke"><div class="genericon genericon-feed"></div>Subscribe</a>
+		<li class="turquoise"><a href="mailto:kev@inburke.com"><div class="genericon genericon-mail"></div>Contact Me</a>
+	</ul>
+
+	<a class="hidden-phone" href="/">&#8592; Back to the homepage</a>
 </div>
