138af754dddb5463c3a7b93c00623102d5a4d026
Kevin Burke <kevin@twilio.com>
2013-04-06 04:48:25 -0700

    make image clickable.

--- blurb.php
+++ blurb.php
@@ -1,5 +1,7 @@
 <div id="blurb" class="clearfix">
-	<img id="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin Burke" />
+  <a href="/" title="Back to the homepage"><img id="logo" src="<?php echo
+    get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin
+    Burke" /></a>
   <span id="blurb-text">
 	Hey, I'm Kevin. I build great experiences. Currently I work at <a
 	href="http://www.twilio.com">Twilio</a>. <a href="/about">More
