fefd7c42fffddd103e9411d666f43a72c2ecc210
Kevin Burke <kevin@twilio.com>
2013-04-08 07:07:22 -0700

    update blurb

--- blurb.php
+++ blurb.php
@@ -1,11 +1,15 @@
-<div id="blurb" class="clearfix">
-  <a href="/" title="Back to the homepage"><img id="logo" src="<?php echo
-    get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin
-    Burke" /></a>
-  <span id="blurb-text">
-	Hey, I'm Kevin. I build great experiences. Currently I work at <a
-	href="http://www.twilio.com">Twilio</a>. <a href="/about">More
-	about me</a>
-  </span>
+<div id="blurb" class="row clearfix">
+    <div class="span3">
+        <div>
+            <a href="/" title="Back to the homepage"><img id="logo" src="<?php echo
+                get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin
+                Burke" /></a>
+        </div>
+        <div  id="blurb-text">
+            Hey, I'm Kevin. I build great experiences. Currently I work at <a
+            href="http://www.twilio.com">Twilio</a>. <a href="/about">More
+            about me</a>
+        </div>
+    </div>
 </div>
 
