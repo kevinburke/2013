766db1712b479092bba9f6f9088f3c3411b39168
Kevin Burke <kevin@twilio.com>
2013-04-09 00:43:32 -0700

    track site speed for all visitors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-var _gaq=[['_setAccount','UA-566397-2'],['_trackPageview']];
+var _gaq = _gaq || [];
+_gaq.push(['_setSiteSpeedSampleRate', 100]);
+_gaq.push(['_setAccount','UA-566397-2'],['_trackPageview']);
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 s.parentNode.insertBefore(g,s)}(document,'script'));
