cc4d87842930e4f94f37a08649eaee6a21f48f73
Kevin Burke <kevin@twilio.com>
2013-04-10 08:43:02 -0700

    watch the blog

--- observr.rb
+++ observr.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env observr
 
 watch( 'scss/(license|normalize|main|colors|genericons)\.scss' )  {|md| cmd = "sass --style compressed scss/main.scss > style.css"; puts cmd; system(cmd) }
+watch( 'scss/blog\.scss' )  {|md| cmd = "sass --style compressed scss/blog.scss > css/blog.css"; puts cmd; system(cmd) }
 
