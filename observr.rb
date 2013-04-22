9f73bc5d55daf03c47a1d43d7d67ffa7429d9bb0
Kevin Burke <kevin@twilio.com>
2013-04-22 01:15:23 -0700

    Play with the colors a little

--- observr.rb
+++ observr.rb
@@ -1,5 +1,18 @@
 #!/usr/bin/env observr
 
-watch( 'scss/(license|normalize|main|colors|genericons)\.scss' )  {|md| cmd = "sass --style compressed scss/main.scss > style.css"; puts cmd; system(cmd) }
-watch( 'scss/blog\.scss' )  {|md| cmd = "sass --style compressed scss/blog.scss > css/blog.css"; puts cmd; system(cmd) }
+main = "sass --style compressed scss/main.scss > style.css"
+home = "sass --style compressed scss/home.scss > css/home.css"
+blog = "sass --style compressed scss/blog.scss > css/blog.css"
+core = "sass --style compressed /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shCore.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shCore.css"
+rdark = "sass --style compressed /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shThemeRDark.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shThemeRDark.css"
 
+watch( 'scss/(license|normalize|main|colors|genericons)\.scss' )  {|md| puts main; system(main) }
+watch( 'scss/blog\.scss' )  {|md| puts blog; system(blog) }
+watch( 'scss/home\.scss' )  {|md| puts home; system(home) }
+
+watch( 'scss/colors\.scss' )  {|md| 
+  [main, blog, home, core, rdark].each do |css_file|
+    puts css_file
+    system css_file
+  end
+}
--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,9 @@
 <div class="span3 sidebar">
 	<?php include_once('blurb.php'); ?>
 	<ul class="link-menu hidden-phone">
-		<li class="yellow"><a href="https://twitter.com/ekrubnivek"><div class="genericon genericon-twitter"></div>Twitter</a>
+		<li class="red"><a href="https://twitter.com/ekrubnivek"><div class="genericon genericon-twitter"></div>Twitter</a>
 		<li class="purple"><a href="https://github.com/kevinburke"><div class="genericon genericon-github"></div>Github</a>
-		<li class="orange"><a href="http://feeds.feedburner.com/kburke"><div class="genericon genericon-feed"></div>Subscribe</a>
+		<li class="yellow"><a href="http://feeds.feedburner.com/kburke"><div class="genericon genericon-feed"></div>Subscribe</a>
 		<li class="turquoise"><a href="mailto:kev@inburke.com"><div class="genericon genericon-mail"></div>Contact Me</a>
 	</ul>
 	<a class="hidden-phone" href="/">&#8592; Back to the homepage</a>
