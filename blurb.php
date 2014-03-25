dfdf5eb37f9e6e71eabcd6f213674f2f4dfa4bc6
Kevin Burke <kev@inburke.com>
2014-03-25 10:32:33 +0800

    load resources over https

--- blurb.php
+++ blurb.php
@@ -1,13 +1,13 @@
 <div id="blurb" class="clearfix">
     <div>
         <a href="/" title="Back to the homepage">
-            <img id="logo" 
+            <img id="logo"
                 src="<?php echo get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin Burke" />
         </a>
     </div>
     <div  id="blurb-text">
         Hey, I'm Kevin. I build great experiences. Currently I work at <a
-        href="http://www.twilio.com">Twilio</a>. <a href="/about">More
+        href="https://www.twilio.com">Twilio</a>. <a href="/about">More
         about me</a>
     </div>
 </div>
