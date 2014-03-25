04de1700019e3048708ba1a3b3a9ee51b9e40efc
Kevin Burke <kev@inburke.com>
2014-03-25 10:34:02 +0800

    upgrade links on the homepage to HTTPS

--- home.php
+++ home.php
@@ -18,7 +18,7 @@ get_header();
 				<li><a href="/kevin/site-redesign/">Tips for designing your personal site</a>
 				<li><a href="/kevin/the-best-ways-to-find-bugs-in-your-code/">Why code review beats testing</a>
 				<li><a href="/kevin/site-redesign/">Reddit's database has two tables</a>
-				<li><a href="http://kevinburke.bitbucket.org/markdowncss/">Markdown.css - let's make the Web prettier</a>
+				<li><a href="https://kevinburke.bitbucket.org/markdowncss/">Markdown.css - let's make the Web prettier</a>
 			</ul>
 		</div>
 		<div class="accordion">
@@ -27,7 +27,7 @@ get_header();
 				Start Here</h3>
 			<ul class="accordion-items hidden-phone">
 				<li><a href="/kevin/the-real-reasons-you-cant-figure-out-how-to-code/">How beginning programmers should read a quickstart guide</a>
-				<li><a href="http://www.twilio.com/engineering/2012/01/18/dont-skimp-on-documentation">How to write documentation for users that don't read</a>
+				<li><a href="https://www.twilio.com/engineering/2012/01/18/dont-skimp-on-documentation">How to write documentation for users that don't read</a>
 				<li><a href="/kevin/client-library-design/">Designing an API Client Library</a>
 				<li><a href="/kevin/how-to-talk-to-recruiters-at-a-career-fair/">How to talk to career fair recruiters</a>
 				<li><a href="/kevin/cmcs-website-redesign-the-good-the-bad-and-the-ugly/">Breaking down my school's website redesign</a>
@@ -50,13 +50,13 @@ get_header();
 					<a href="https://github.com/kevinburke"><div class="genericon genericon-github"></div>Github</a>
 				</div>
 				<div class="square">
-					<a href="http://bitbucket.org/kevinburke">Bitbucket</a>
+					<a href="https://bitbucket.org/kevinburke">Bitbucket</a>
 				</div>
 				<div class="square">
 					<a href="https://plus.google.com/111464885298476091898"><div class="genericon genericon-googleplus"></div>Google+</a>
 				</div>
 				<div class="square">
-					<a href="http://linkedin.com/in/ekrub"><div class="genericon genericon-linkedin"></div>LinkedIn</a>
+					<a href="https://linkedin.com/in/ekrub"><div class="genericon genericon-linkedin"></div>LinkedIn</a>
 				</div>
 			</div>
 		</div>
@@ -68,7 +68,7 @@ get_header();
 			<ul class="accordion-items hidden-phone">
 				<li class="nopointer">has used a DVORAK keyboard since high school</li>
 				<li>
-				<a href="http://kev.inburke.com/kevin/escaping-from-your-email/">
+				<a href="https://kev.inburke.com/kevin/escaping-from-your-email/">
 					downloads email only once every 4 hours
 				</a>
 				</li>
@@ -79,7 +79,7 @@ get_header();
 				</li>
 				<li class="nopointer">emailed his entire high school about how to use apostrophes</li>
 				<li>
-				<a href='http://www.youtube.com/watch?v=sdMoE7mr9H0'>
+				<a href='https://www.youtube.com/watch?v=sdMoE7mr9H0'>
 					made a 10-minute video on myths about foreign aid
 				</a>
 				</li>
@@ -91,13 +91,13 @@ get_header();
 			<h3 id="twitter" class="accordion-heading toggle">
 				<div class="genericon genericon-twitter"></div>
 				Twitter</h3>
-			<a class="twitter-timeline" 
-				data-chrome="noheader noborders transparent nofooter" 
-				data-link-color="#01d2ff" 
+			<a class="twitter-timeline"
+				data-chrome="noheader noborders transparent nofooter"
+				data-link-color="#01d2ff"
 				width="560"
 				height="300"
-				data-dnt="true" 
-				href="https://twitter.com/ekrubnivek" 
+				data-dnt="true"
+				href="https://twitter.com/ekrubnivek"
 				data-widget-id="318106551330148352"></a>
 			</li>
 		</div>
@@ -115,7 +115,7 @@ get_header();
 			</h3>
 			<ul class="accordion-items hidden-phone">
 				<li>
-				<a href="http://github.com/kevinburke/tecate">
+				<a href="https://github.com/kevinburke/tecate">
 					Tecate.js - making it easier to spot syntax problems in HTML
 				</a>
 				</li>
@@ -125,7 +125,7 @@ get_header();
 				</a>
 				</li>
 				<li>
-				<a href="http://kevinburke.bitbucket.org/markdowncss">
+				<a href="https://kevinburke.bitbucket.org/markdowncss">
 					Markdown.css - easy, beautiful CSS for your Markdown files
 				</a>
 				</li>
@@ -135,7 +135,7 @@ get_header();
 				</a>
 				</li>
 				<li>
-				<a href="http://rssafter5.appspot.com">
+				<a href="https://rssafter5.appspot.com">
 					RSS After 5 - scheduled RSS delivery
 				</a>
 				</li>
