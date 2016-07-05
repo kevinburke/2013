74d1574c08aee6b4be866790dec6ffacce03abff
Kevin Burke <kev@inburke.com>
2016-07-05 09:51:19 -0700

    Use HTTPS for github links

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" role="contentinfo">
       &copy; 2006 - <?php echo date('Y'); ?> Kevin Burke. <br />
       Cited as <a href="http://qr.ae/F5EKw">one of the best personal websites on the Net</a>.
-      View the <a href="https://github.com/kevinburke/2013">source code</a>.
+      View the <a href="https://github.com/kevinburke/2013">source code</a>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
