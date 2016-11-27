a8c4996472b6939ade55804c75b3b9c505444495
Kevin Burke <kev@inburke.com>
2016-11-26 22:15:11 -0800

    change my image

--- blurb.php
+++ blurb.php
@@ -2,7 +2,7 @@
     <div>
         <a href="/" title="Back to the homepage">
             <img id="logo"
-                src="<?php echo get_stylesheet_directory_uri(); ?>/img/kevin-mask-blue.png" alt="Kevin Burke" />
+                src="<?php echo get_stylesheet_directory_uri(); ?>/img/kevin-mask-2-small.png" alt="Kevin Burke" />
         </a>
     </div>
     <div id="blurb-text">
