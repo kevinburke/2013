2319ea3ffaae46e042c759e6218189ac89729497
Kevin Burke <kev@inburke.com>
2018-09-01 18:01:21 -0700

    half padding below, half above

--- observr.rb
+++ observr.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env observr
 
-main = "sass --style compressed scss/main.scss > style.css"
-home = "sass --style compressed scss/home.scss > css/home.css"
-blog = "sass --style compressed scss/blog.scss > css/blog.css"
-twitter = "sass --style compressed scss/twitter.scss > css/twitter.css"
-core = "sass --style compressed /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shCore.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shCore.css"
-rdark = "sass --style compressed /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shThemeRDark.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shThemeRDark.css"
+main = "sass --style expanded scss/main.scss > style.css"
+home = "sass --style expanded scss/home.scss > css/home.css"
+blog = "sass --style expanded scss/blog.scss > css/blog.css"
+twitter = "sass --style expanded scss/twitter.scss > css/twitter.css"
+core = "sass --style expanded /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shCore.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shCore.css"
+rdark = "sass --style expanded /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/scss/shThemeRDark.scss > /Users/kevin/code/syntaxhighlighter/trunk/syntaxhighlighter3/styles/shThemeRDark.css"
 
 watch( 'scss/(license|normalize|main|colors|genericons)\.scss' )  {|md| puts main; system(main) }
 watch( 'scss/blog\.scss' )  {|md| puts blog; system(blog) }
